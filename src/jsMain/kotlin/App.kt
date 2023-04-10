0d8c91651aeb6a401486c699787503d267b5b684
Konstantin Kalinin <Okkkstantin@mail.ru>
2023-04-10 23:28:17 +0600

    подключил монго

--- src/jsMain/kotlin/App.kt
+++ src/jsMain/kotlin/App.kt
@@ -1,9 +1,12 @@
+import components.CTest
 import react.FC
 import react.Props
 import react.create
 import react.dom.client.createRoot
 import react.dom.html.ReactHTML.li
 import react.dom.html.ReactHTML.ol
+import tanstack.query.core.QueryClient
+import tanstack.react.query.QueryClientProvider
 
 fun main() {
     val container = web.dom.document.getElementById("root")!!
@@ -11,11 +14,8 @@ fun main() {
 }
 
 val app = FC<Props>("App") {
-    ol {
-        for (i in "hello, world!".indices) {
-            li {
-                +"hello, world!"[i].toString()
-            }
-        }
+    QueryClientProvider {
+        client = QueryClient()
+        CTest{}
     }
 }
